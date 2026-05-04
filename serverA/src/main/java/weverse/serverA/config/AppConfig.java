bf966b6eaa0259e344db0c2112d6f7416407c4e2
201912025 <michi2012@naver.com>
2026-05-04 17:05:51 +0900

    feat: AppConfig 스케쥴링 빈 설정 별도  SchedulingConfig로 변경

--- serverA/src/main/java/weverse/serverA/config/AppConfig.java
+++ serverA/src/main/java/weverse/serverA/config/AppConfig.java
@@ -2,10 +2,8 @@ package weverse.serverA.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAsync      // EventNotifier의 @Async 작동을 위해 필수
-@EnableScheduling // 스케줄러 작동을 위해 필수
 public class AppConfig {
 }
