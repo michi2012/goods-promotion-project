08b98a58fa67714a47364a1ba655325e0fa4c999
201912025 <michi2012@naver.com>
2026-05-06 09:06:41 +0900

    refactor: 스케쥴러 빈 별도 클래스로 분리

--- serverB/src/main/java/weverse/serverB/config/AppConfig.java
+++ serverB/src/main/java/weverse/serverB/config/AppConfig.java
@@ -3,12 +3,8 @@ package weverse.serverB.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
-@EnableScheduling
 public class AppConfig {
 
     @Bean
@@ -16,13 +12,4 @@ public class AppConfig {
         return new ObjectMapper();
     }
 
-    // 스케줄러 전용 스레드 풀
-    @Bean
-    public TaskScheduler taskScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(10); // 적절한 스레드 수 유지
-        scheduler.setThreadNamePrefix("ToCWorker-");
-        scheduler.initialize();
-        return scheduler;
-    }
 }
