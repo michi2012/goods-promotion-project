5ddc47dbd362b016d35693a4bf432ae9d294f9e9
201912025 <michi2012@naver.com>
2026-05-23 13:40:56 +0900

    build: 카프카 의존성 추가

--- serverA/src/main/java/weverse/serverA/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package weverse.serverA.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync      // EventNotifier의 @Async 작동을 위해 필수
-public class AppConfig {
-}
